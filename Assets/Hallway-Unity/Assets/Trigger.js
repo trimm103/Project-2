72ececb669afd59696ea62a5b540c9ede3ce7ac3
Shae Trimmer <trimm103@mail.chapman.edu>
2015-04-28 18:39:41 -0700

    Added fun stuff

---
+++ Assets/Hallway-Unity/Assets/Trigger.js
@@ -0,0 +1,55 @@
+﻿var face : GameObject;
+
+var hasplayed = false;
+var entertrigger = false;
+
+var screamingsound : AudioClip;
+
+
+
+
+function Start () {
+
+	entertrigger = false;
+	face.renderer.enabled = false;
+
+}
+
+
+function OnTriggerEnter (other : Collider) {
+
+	entertrigger = true;
+}
+
+
+function Update () {
+
+	if (entertrigger == true) { 
+		face.renderer.enabled = true;
+		removeovertime ();
+		makehimscream ();
+		
+	}
+}
+
+
+
+
+function removeovertime () {
+	yield WaitForSeconds (1.5);
+	face.renderer.enabled = false;
+	Destroy(face);
+	Application.Quit();
+	
+}
+
+
+function makehimscream () {
+	if (!hasplayed) {
+		hasplayed = true;
+		audio.PlayOneShot(screamingsound);
+	
+	}
+
+
+}
