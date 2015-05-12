fbf944b39fd04b5c56114eb099297be28bb9cda6
Shae Trimmer <trimm103@mail.chapman.edu>
2015-05-12 10:28:14 -0700

    Spider falls in front of door, working on cutscene

--- Assets/Hallway-Unity/Assets/CameraSwitch.js
+++ Assets/Hallway-Unity/Assets/CameraSwitch.js
@@ -3,8 +3,17 @@
 var camera1 : Camera;
 var camera2 : Camera;
 
-function Start () {
+var cameraAnim : Animator;
 
+var sound1 : AudioClip;
+var sound2 : AudioClip;
+
+var player : GameObject;
+
+function Start () {
+	player = GameObject.Find("Player");
+	cameraAnim = GameObject.Find("CinematicCamera").GetComponent("Animator");
+	cameraAnim.enabled = false;
 }
 
 function Update () {
@@ -16,7 +25,16 @@ function OnTriggerEnter (other : Collider) {
 
 	GameObject.Find("Player").GetComponent(CharacterMotor).enabled = false;
 	GameObject.Find("Player").GetComponent(MouseLook).enabled = false;
+	yield WaitForSeconds (1.5);
 
 	camera1.camera.enabled = false;
 	camera2.camera.enabled = true;
+	player.transform.position = Vector3(345,5,86);
+	cameraAnim.enabled = true;
+	audio.PlayOneShot(sound1);
+	yield WaitForSeconds (2.9);
+	cameraAnim.enabled = false;
+	audio.PlayOneShot(sound2);
+	
+
 }
---
+++ Assets/Hallway-Unity/Assets/SpiderTrigger.js
@@ -0,0 +1,65 @@
+﻿
+var hasplayed = false;
+var entertrigger = false;
+
+var spiderSound : AudioClip;
+var spiderAnim : Animator;
+
+
+
+function Start () {
+
+	entertrigger = false;
+	spiderAnim = GameObject.Find("spider").GetComponent("Animator");
+	spiderAnim.enabled = false;
+
+
+}
+
+
+function OnTriggerEnter (other : Collider) {
+
+	entertrigger = true;
+	GameObject.Find("Player").GetComponent(CharacterMotor).enabled = false;
+	
+	spiderAnim.enabled = true;
+
+}
+
+
+function Update () {
+
+	if (entertrigger == true) { 
+
+		removeovertime ();
+		spiderScream ();
+		
+	}
+	
+}
+
+
+
+
+function removeovertime () {
+	yield WaitForSeconds (1.5);
+
+
+	GameObject.Find("Player").GetComponent(CharacterMotor).enabled = true;
+	yield WaitForSeconds (1.0);
+	spiderAnim.enabled = false;
+	Destroy(this);
+
+	
+}
+
+
+function spiderScream () {
+	if (!hasplayed) {
+		hasplayed = true;
+		audio.PlayOneShot(spiderSound);
+	
+	}
+
+
+}
